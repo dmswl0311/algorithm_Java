19d57be0e0f2f50595316ad522602f51e31fdb80
eunji <dmswl_0311@naver.com>
2021-07-20 00:29:22 +0900

    10950

---
+++ 기본입출력/BOJ_10950.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int t=scan.nextInt();
+		for (int i = 0; i<t; i++) {
+			int A=scan.nextInt();
+			int B=scan.nextInt();
+			System.out.println(A+B);
+		}
+		scan.close();
+	}
+
+}
