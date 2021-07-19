662488eac69f6749ad32e4a7155292843f1b0e2f
eunji <dmswl_0311@naver.com>
2021-07-20 00:25:56 +0900

    init

---
+++ 기본입출력/BOJ_2558.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_2558 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+				Scanner scan=new Scanner(System.in);
+				int A=scan.nextInt();
+				int B=scan.nextInt();
+				System.out.println(A+B);
+				scan.close();
+	}
+
+}
