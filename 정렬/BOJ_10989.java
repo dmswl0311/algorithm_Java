c6cb8b24b17e8feea4aa44588a872423973135eb
eunji <dmswl_0311@naver.com>
2021-07-22 23:06:29 +0900

    10989

---
+++ 정렬/BOJ_10989.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_10989 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int N=Integer.parseInt(bf.readLine());
+		int[] arr=new int[10001]; // 계수정렬을 위해 배열 생성
+		int max=0;
+		
+		for (int i=0; i<N; i++) {
+			st=new StringTokenizer(bf.readLine());
+			int num=Integer.parseInt(st.nextToken());
+			arr[num]+=1;
+			if (max<num) {
+				max=num;
+			}
+		}
+		for (int i=0; i<max+1; i++) {
+			if (arr[i]!=0) {
+				// 값이 존재한다면 
+				for (int r=0; r<arr[i]; r++) {
+					// i를 st[i](i의 개수)만큼 반복해서 출력
+					bw.write(i+"\n");
+				}
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+}
