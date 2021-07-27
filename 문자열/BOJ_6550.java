9171afba057b9ac2b7586ecaf1b0587380a78825
eunji <dmswl_0311@naver.com>
2021-07-28 00:19:46 +0900

    6550

--- 문자열/BOJ_6550.java
+++ 문자열/BOJ_6550.java
@@ -4,16 +4,30 @@ public class BOJ_6550 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan=new Scanner(System.in);
-		String s="";
-		while((s=scan.next())!=null) {
-			String t=scan.next();
+		
+		while(scan.hasNext()) {
+			String[] str=scan.nextLine().split(" ");
+			String s=str[0];
+			String t=str[1];
+			String result="Yes";
 			
 			if (t.contains(s)==true) {
 				System.out.println("Yes");
 			}else {
-				System.out.println("No");
+				outer: for(int i=0; i<s.length(); i++) {
+					for (int j=0; j<t.length(); j++) {
+						if (s.charAt(i)==t.charAt(j)) {
+							t=(String) t.subSequence(j+1, t.length());
+							break;
+						}
+						if (j==t.length()-1) {
+							result="No";
+							break outer;
+						}
+					}
+				}
+			System.out.println(result);
 			}
 		}
-
 	}
 }
