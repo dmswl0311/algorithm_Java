ad2e0ce45a63e0498c8979d10b0778b9dfa1f52a
eunji <dmswl_0311@naver.com>
2021-08-20 17:29:53 +0900

    분할정복, 재귀, 괄호 위치 (수정)

--- 재귀/BOJ_1992_쿼드트리.java
+++ 재귀/BOJ_1992_쿼드트리.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
+/**
  * @see https://www.acmicpc.net/problem/1992
  * 분할정복, 재귀
+ * 괄호 묶어주는 부분 **
  */
+
 public class BOJ_1992_쿼드트리 {
 	static StringBuilder sb=new StringBuilder();
 	static StringTokenizer st;
 	static int[][] map;
-	static int cnt_zero;
-	static int cnt_one;
 	static int cnt;
 	static int N;
 	static String str;
@@ -28,56 +28,37 @@ public class BOJ_1992_쿼드트리 {
 			}
 		}
 		cnt=0;
-		if(N==1) {
-			System.out.println("1");
-		}else {
-			str="(";
-			
-			divide(0,0,N);
-			
-			str=str.concat(")");
-			
-			while(true) {
-				if (str.contains("(0000)") || str.contains("(1111)")) {
-					str=str.replace("(0000)", "0");
-					str=str.replace("(1111)", "1");
-				}else {
-					break;
-				}
-			}
-			
-			System.out.println(str);
-		}
-		
+		divide(0,0,N);
+		System.out.println(sb);
 	}
 
 	private static void divide(int x, int y, int n) {
-		if(n==2) {
-			for(int i=x; i<x+2; i++) {
-				for (int j=y; j<y+2; j++) {
-					str=str.concat(String.valueOf(map[i][j]));
-				}
+		cnt=0;
+		
+		// 어떤 수로 이루어져 있는지 알기 위해 cnt에 현재 값 저장
+		for (int i=x; i<x+n; i++) {
+			for (int j=y; j<y+n; j++) {
+				cnt+=map[i][j];
 			}
-			return;
 		}
 		
-		int len=n/2;
-		
-		str=str.concat("(");
-		divide(x,y,len);
-		str=str.concat(")");
-		
-		str=str.concat("(");
-		divide(x,y+len,len);
-		str=str.concat(")");
-		
-		str=str.concat("(");
-		divide(x+len,y,len);
-		str=str.concat(")");
-		
-		str=str.concat("(");
-		divide(x+len,y+len,len);
-		str=str.concat(")");
-		
+		// 기저조건이 따로 없지만, if~else문이 기저조건
+		if (cnt==n*n) {
+			// 만약, cnt 값이 넓이와 같다면, 1로 채워져있음
+			sb.append(1);
+		}else if (cnt==0) {
+			// cnt 값이 0과 같다면, 0으로 채워져있음
+			sb.append(0);
+		}
+		else {
+			// 1과 0이 섞여있다면, 괄호로 묶어주고 다시 4분할
+			sb.append("(");
+			int len=n/2;
+			divide(x,y,len);
+			divide(x,y+len,len);
+			divide(x+len,y,len);
+			divide(x+len,y+len,len);
+			sb.append(")");
+		}
 	}
 }
